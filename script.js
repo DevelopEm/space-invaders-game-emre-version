04f5519d06529340fca0a346a242510b9c8391f9
DevelopEm <155112752+DevelopEm@users.noreply.github.com>
2025-02-08 12:57:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -254,10 +254,10 @@ function detectCollisions() {
             score += 10; // Increase score
             if (checkWin()) {
               level++;
-              invaderSpeed = Math.min(invaderSpeed + 0.1, 1); // Increase speed as levels go up, up to a max speed
+              invaderSpeed = Math.min(invaderSpeed + 0.3, 3); // Increase speed as levels go up, up to a max speed
               if (level <= 10) {
                 invaderRowCount = Math.min(invaderRowCount + 1, 4); // Increase rows slightly
-                invaderColumnCount = Math.min(invaderColumnCount + 1, 7); // Increase columns slowly
+                invaderColumnCount = Math.min(invaderColumnCount + 1, 10); // Increase columns slowly
               }
               createInvaders();  // Regenerate the invaders with updated count and speed
             }
